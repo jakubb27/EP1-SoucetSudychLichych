21ef2c89fce0efb8653d3e9b959f2bbf0a20cb09
jakubb27 <Navratil.Jakub@stud.vassboskovice.cz>
2024-06-10 13:34:41 +0200

    úkol 5

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,11 +1,34 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
-        Console.WriteLine("Definuj poèet zadávaných èísel")
-        Dim èíslo As Integer
-        èíslo = Console.ReadLine()
+    Sub Main(args As String())
+        While True
+            Dim celkovéèíslo As Integer = GetTotalNumbers()
+            Dim lichýsouèet As Integer = 0
+            Dim sudýsouèet As Integer = 0
+            Dim Lichýpoèet As Integer = 0
+            Dim sudýpoèet As Integer = 0
 
+            For i As Integer = 1 To celkovéèíslo
+                Dim number As Integer = GetNumber(i)
+                If number Mod 2 = 0 Then
+                    sudýsouèet += number
+                    sudýpoèet += 1
+                Else
+                    lichýsouèet += number
+                    Lichýpoèet += 1
+                End If
+            Next
+            Console.WriteLine($"Souèet lichých èísel: {lichýsouèet}")
+            Console.WriteLine($"Poèet lichých èísel: {Lichýpoèet}")
+            Console.WriteLine($"Souèet sudých èísel: {sudýsouèet}")
+            Console.WriteLine($"Poèet sudých èísel: {sudýpoèet}")
 
+            Console.WriteLine("Chcete zadat další èísla? (a/n)")
+            If Console.ReadLine().ToLower() <> "a" Then
+                Exit While
+            End If
+        End While
     End Sub
 End Module
+
